10af8c914764c7d1bb1eac18609f9ac997e2bbf9
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-30 10:05:08 +0300

    feat(internal.handlers): add auth handlers

---
+++ internal/handlers/auth.go
@@ -0,0 +1,95 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/mksmstpck/restoracio/pkg/models"
+	"github.com/mksmstpck/restoracio/pkg/utils"
+	"github.com/pborman/uuid"
+)
+
+func (h *Handlers) login(c *gin.Context) {
+	var creds *models.Login
+
+	if err := c.ShouldBindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Info("handlers.LogInByEmail: ", err)
+		return
+	}
+
+	admin, err := h.db.AdminGetByEmail(creds.Email)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, models.Message{Message: err.Error()})
+		log.Error("handlers.LogInByEmail: ", err)
+		return
+	}
+
+	password, err := h.db.AdminGetPasswordById(uuid.Parse(admin.ID))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, models.Message{Message: err.Error()})
+		log.Error("handlers.LogInByUsername: ", err)
+		return
+	}
+
+	if ok := utils.CheckPasswordHash(creds.Password, password); ok != true {
+		c.JSON(http.StatusBadRequest, models.Message{Message: "invalid password"})
+		log.Info("handlers.LogInByEmail: invalid password")
+		return
+	}
+
+	access, err := utils.CreateJWT(h.access_exp, h.access_secret, uuid.Parse(admin.ID))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Error("handlers.LogInByEmail: ", err)
+		return
+	}
+
+	refresh, err := utils.CreateJWT(h.refresh_exp, h.refresh_secret, uuid.Parse(admin.ID))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Error("handlers.LogInByEmail: ", err)
+		return
+	}
+	c.Header("access", access)
+	c.Header("refresh", refresh)
+
+	c.JSON(http.StatusNoContent, nil)
+	log.Info("handlers.LogInByEmail: user logged in")
+}
+
+func (h *Handlers) refresh(c *gin.Context) {
+	admin_id, err := utils.ValidateJWT(c.Request.Header.Get("refresh"), h.refresh_secret)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Error("handlers.Refresh: ", err)
+		return
+	}
+	admin, err := h.db.AdminGetByID(admin_id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Error("handlers.Refresh: ", err)
+		return
+	}
+
+	access, err := utils.CreateJWT(h.access_exp, h.access_secret, uuid.Parse(admin.ID))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Error("handlers.LogInByUsername: ", err)
+		return
+	}
+
+	refresh, err := utils.CreateJWT(h.refresh_exp, h.refresh_secret, uuid.Parse(admin.ID))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.Message{Message: err.Error()})
+		log.Error("handlers.LogInByUsername: ", err)
+		return
+	}
+	c.Header("access", access)
+	c.Header("refresh", refresh)
+
+	c.JSON(http.StatusNoContent, nil)
+	log.Info("handlers.Refresh: user logged in")
+}
