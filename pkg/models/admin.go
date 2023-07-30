39db90c8074302194fc4000d42ef23a7b70583c6
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-30 10:45:36 +0300

    fix(pkg.models): change json tag in admin struct

--- pkg/models/admin.go
+++ pkg/models/admin.go
@@ -1,7 +1,7 @@
 package models
 
 type Admin struct {
-	ID           string `json:"_id" bun:"id,pk"`
+	ID           string `json:"id" bun:"id,pk"`
 	Name         string `json:"name" binding:"required"`
 	Email        string `json:"email" binding:"required"`
 	Password     string `json:"password" binding:"required"`
