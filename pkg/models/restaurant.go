e1e35909d4bfc5b998d4fb5087fc2b1a938dd30f
Max Ostapchuck <mksmstpck@gmail.com>
2023-07-30 23:02:32 +0300

    refactor(pkg.models): change some names and types

--- pkg/models/restaurant.go
+++ pkg/models/restaurant.go
@@ -4,8 +4,8 @@ type Restaurant struct {
 	ID       string   `json:"id" bun:",pk"`
 	Name     string   `json:"name" binding:"required"`
 	Location string   `json:"location" binding:"required"`
-	Staff    []string `json:"staff" binding:"required"`
-	Dish     []string `json:"dish" binding:"required"`
-	Table    []string `json:"table" binding:"required"`
-	AdminID  []string `json:"admin_id"`
+	StaffIDs []string `json:"staff" binding:"required"`
+	DishIDs  []string `json:"dish" binding:"required"`
+	TableIDs []string `json:"table" binding:"required"`
+	AdminID  string   `json:"admin_id"`
 }
