3e28972d3cb77a98ee86df44a285c569c53984d2
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-10 23:44:20 +0300

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,22 +12,16 @@ type Config struct {
 	CockDNS       string
 	CacheExpire   time.Duration
 	CachePurge    time.Duration
-	AccessSecret  []byte
-	RefreshSecret []byte
-	AccessExp     time.Duration
-	RefreshExp    time.Duration
+	TokenSecret []byte
+	TokenExp    time.Duration
 }
 
 func NewConfig() Config {
-	accessExp, err := strconv.Atoi(os.Getenv("ACCESS_EXP"))
+	tokenExp, err := strconv.Atoi(os.Getenv("TOKEN_EXP"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	refreshExp, err := strconv.Atoi(os.Getenv("REFRESH_EXP"))
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	cacheExp, err := strconv.Atoi(os.Getenv("CACHE_EXP"))
 	if err != nil {
@@ -35,14 +29,15 @@ func NewConfig() Config {
 	}
 
 	cachePurge, err := strconv.Atoi(os.Getenv("CACHE_PURGE"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	return Config{
 		GinUrl:        os.Getenv("GIN_URL"),
 		CockDNS:       os.Getenv("COCK_DNS"),
 		CacheExpire:   time.Duration(cacheExp),
 		CachePurge:    time.Duration(cachePurge),
-		AccessSecret:  []byte(os.Getenv("ACCESS_SECRET")),
-		RefreshSecret: []byte(os.Getenv("REFRESH_SECRET")),
-		AccessExp:     time.Duration(accessExp),
-		RefreshExp:    time.Duration(refreshExp),
+		TokenSecret:   []byte(os.Getenv("TOKEN_SECRET")),
+		TokenExp:      time.Duration(tokenExp),
 	}
 }
