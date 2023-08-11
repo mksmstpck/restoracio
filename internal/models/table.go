6430337cc09f707f6de78f54e185f1057c6772e3
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-11 19:17:00 +0300

    feat(internal.models): add bun tags

--- internal/models/table.go
+++ internal/models/table.go
@@ -2,10 +2,11 @@ package models
 
 type Table struct {
 	ID           string       `json:"id" bun:",pk"`
-	Number       int          `json:"number"`
-	Placement    string       `json:"placement"`
-	MaxPeople    int          `json:"max_people"`
-	IfReserved   bool         `json:"if_reserved"`
-	RestaurantID string       `json:"restaurant_id"`
+	Number       int          `json:"number" bun:"number"`
+	Placement    string       `json:"placement" bun:"placement"`
+	MaxPeople    int          `json:"max_people" bun:"max_people"`
+	IsReserved   bool         `json:"is_reserved" bun:"is_reserved"`
+	IsOccupied   bool         `json:"is_occupied" bun:"is_occupied"`
+	RestaurantID string       `json:"restaurant_id" bun:"restaurant_id"`
 	Reservation  *Reservation `json:"reservation" bun:"rel:has-one"`
 }
