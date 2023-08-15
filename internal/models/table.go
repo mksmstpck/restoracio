2c1d658a0c40ce95110a771fcc8ba47dffe8aec3
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-15 21:43:06 +0300

    not important changes

--- internal/models/table.go
+++ internal/models/table.go
@@ -2,11 +2,11 @@ package models
 
 type Table struct {
 	ID           string       `json:"id" bun:",pk"`
-	Number       int          `json:"number" bun:"number"`
-	Placement    string       `json:"placement" bun:"placement"`
-	MaxPeople    int          `json:"max_people" bun:"max_people"`
+	Number       int          `json:"number" binding:"required" bun:"number"`
+	Placement    string       `json:"placement" binding:"required" bun:"placement"`
+	MaxPeople    int          `json:"max_people" binding:"required" bun:"max_people"`
 	IsReserved   bool         `json:"is_reserved" bun:"is_reserved"`
-	IsOccupied   bool         `json:"is_occupied" bun:"is_occupied"`
+	IsOccupied   bool         `json:"is_occupied" binding:"required" bun:"is_occupied"`
 	RestaurantID string       `json:"restaurant_id" bun:"restaurant_id"`
 	Reservation  *Reservation `json:"reservation" bun:"rel:has-one"`
 }
