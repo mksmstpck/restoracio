1b8a0eceb7e6e84458b78caaf16c7530e4396ba4
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-20 18:58:13 +0300

    feat(internal.models): make it work

--- internal/models/menu.go
+++ internal/models/menu.go
@@ -4,7 +4,7 @@ type Menu struct {
 	ID           string  `json:"id" bun:"id,pk"`
 	Name         string  `json:"name" binding:"required"`
 	Description  string  `json:"description" binding:"required"`
-	DishIDs      []*Dish `json:"dish" bun:"rel:has-many,join:id=menu_id"`
+	Dishes      []*Dish `json:"dish" bun:"rel:has-many,join:id=menu_id"`
 	QRCode       []byte  `json:"qrcode" bun:"qrcode"`
 	RestaurantID string  `json:"restaurant_id" bun:"restaurant_id"`
 }
