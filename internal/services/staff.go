176913d9237344327301ed5d734ab80de0657b35
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-23 09:28:24 +0300

    feat(internal.services): add staff

---
+++ internal/services/staff.go
@@ -0,0 +1,81 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/mksmstpck/restoracio/internal/models"
+	"github.com/mksmstpck/restoracio/utils"
+	"github.com/pborman/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+func (s *Services) StaffCreateService(staff models.Staff, admin models.Admin) (models.Staff, error) {
+	if admin.Restaurant == nil {
+		log.Info(utils.ErrRestaurantNotFound)
+		return models.Staff{}, errors.New(utils.ErrRestaurantNotFound)
+	}
+	staff.RestaurantID = admin.Restaurant.ID
+	res, err := s.db.Staff.CreateOne(s.ctx, staff)
+	if err != nil {
+		log.Info(err)
+		return models.Staff{}, err
+	}
+	log.Info("staff created")
+	return res, nil
+}
+
+func (s *Services) StaffGetByIDService(id uuid.UUID, admin models.Admin) (models.Staff, error) {
+	if admin.Restaurant == nil {
+		log.Info(utils.ErrRestaurantNotFound)
+		return models.Staff{}, errors.New(utils.ErrRestaurantNotFound)
+	}
+	staff, err := s.db.Staff.GetByID(s.ctx, id, uuid.Parse(admin.Restaurant.ID))
+	if err != nil {
+		log.Info(err)
+		return models.Staff{}, err
+	}
+	return staff, nil
+}
+
+func (s *Services) StaffGetAllInRestaurantService(admin models.Admin) ([]models.Staff, error) {
+	if admin.Restaurant == nil {
+		log.Info(utils.ErrRestaurantNotFound)
+		return nil, errors.New(utils.ErrRestaurantNotFound)
+	}
+	staff, err := s.db.Staff.GetAllInRestaurant(s.ctx, uuid.Parse(admin.Restaurant.ID))
+	if err != nil {
+		log.Info(err)
+		return nil, err
+	}
+	log.Info("staffs found")
+	return staff, nil
+}
+
+func (s *Services) StaffUpdateService(staff models.Staff, admin models.Admin) error {
+	if admin.Restaurant == nil {
+		log.Info(utils.ErrRestaurantNotFound)
+		return errors.New(utils.ErrRestaurantNotFound)
+	}
+	staff.RestaurantID = admin.Restaurant.ID
+	err := s.db.Staff.UpdateOne(s.ctx, staff)
+	if err != nil {
+		log.Info(err)
+		return err
+	}
+	log.Info("staff updated")
+	return nil
+}
+
+func (s *Services) StaffDeleteService(id uuid.UUID, admin models.Admin) error {
+	if admin.Restaurant == nil {
+		log.Info(utils.ErrRestaurantNotFound)
+		return errors.New(utils.ErrRestaurantNotFound)
+	}
+	err := s.db.Staff.DeleteOne(s.ctx, id, uuid.Parse(admin.Restaurant.ID))
+	if err != nil {
+		log.Info(err)
+		return err
+	}
+	log.Info("staff deleted")
+	return nil
+}
