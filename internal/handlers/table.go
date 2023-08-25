de4a1def7ec5de84d9164667479812c1bf833135
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-25 20:58:57 +0300

    feat(internal.handlers): add swagger comments to table

--- internal/handlers/table.go
+++ internal/handlers/table.go
@@ -10,6 +10,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+//	@Summary		TableCreate
+//	@Security		JWTAuth
+//	@Tags			Table
+//	@Description	creates a new table
+//	@ID				table-create
+//	@Accept			json
+//	@Produce		json
+//	@Param			input	body		models.Table	true	"Table"
+//	@Success		201		{object}	models.Table
+//	@Failure		default	{object}	models.Message
+//	@Router			/table [post]
 func (h *Handlers) tableCreate(c *gin.Context){
 	admin := c.MustGet("Admin")
 	var t models.Table
@@ -28,6 +39,17 @@ func (h *Handlers) tableCreate(c *gin.Context){
 	c.JSON(http.StatusOK, t)
 }
 
+//	@Summary		TableGetByID
+//	@Security		JWTAuth
+//	@Tags			Table
+//	@Description	returns a table by id
+//	@ID				table-get-by-id
+//	@Accept			json
+//	@Produce		json
+//	@Success		200		{object}	models.Table
+//	@Failure		default	{object}	models.Message
+//	@Router			/table/{id} [get]
+//	@Param			id	path	string	true	"Table ID"
 func (h *Handlers) tableGetByID(c *gin.Context){
 	id := uuid.Parse(c.Param("id"))
 	t, err := h.service.TableGetByIDService(id)
@@ -40,6 +62,17 @@ func (h *Handlers) tableGetByID(c *gin.Context){
 	c.JSON(http.StatusOK, t)
 }
 
+//	@Summary		TableGetAllInRestaurant
+//	@Security		JWTAuth
+//	@Tags			Table
+//	@Description	returns all tables in a restaurant
+//	@ID				table-get-all-in-restaurant
+//	@Accept			json
+//	@Produce		json
+//	@Success		200		{object}	models.Table
+//	@Failure		default	{object}	models.Message
+//	@Router			/table/all/{id} [get]
+//	@Param			id	path	string	true	"Table ID"
 func (h *Handlers) tableGetAllInRestaurant(c *gin.Context){
 	id := uuid.Parse(c.Param("id"))
 	t, err := h.service.TableGetAllInRestaurantService(id)
@@ -52,6 +85,17 @@ func (h *Handlers) tableGetAllInRestaurant(c *gin.Context){
 	c.JSON(http.StatusOK, t)
 }
 
+//	@Summary		TableUpdate
+//	@Security		JWTAuth
+//	@Tags			Table
+//	@Description	updates a table
+//	@ID				table update
+//	@Accept			json
+//	@Produce		json
+//	@Param			input	body		models.Table	true	"Table"
+//	@Success		204		{object}	nil
+//	@Failure		default	{object}	models.Message
+//	@Router			/table [put]
 func (h *Handlers) tableUpdate(c *gin.Context){
 	admin := c.MustGet("Admin").(models.Admin)
 	var t models.Table
@@ -80,6 +124,17 @@ func (h *Handlers) tableUpdate(c *gin.Context){
 	c.JSON(http.StatusNoContent, nil)
 }
 
+//	@Summary		TableDelete
+//	@Security		JWTAuth
+//	@Tags			Table
+//	@Description	deletes a table
+//	@ID				table-delete
+//	@Accept			json
+//	@Produce		json
+//	@Success		204		{object}	nil
+//	@Failure		default	{object}	models.Message
+//	@Router			/table/{id} [delete]
+//	@Param			id	path	string	true	"Table ID"
 func (h *Handlers) tableDelete(c *gin.Context){
 	id := uuid.Parse(c.Param("id"))
 	admin := c.MustGet("Admin").(models.Admin)
