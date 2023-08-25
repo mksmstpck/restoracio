7a585fce39c98be0a3b54ea1fcfc31be54ad13a2
Max Ostapchuck <mksmstpck@gmail.com>
2023-08-25 20:59:14 +0300

    feat(internal.handlers): add swagger comments to staff

--- internal/handlers/staff.go
+++ internal/handlers/staff.go
@@ -10,6 +10,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+//	@Summary		StaffCreate
+//	@Security		JWTAuth
+//	@Tags			Staff
+//	@Description	creates a new employee
+//	@ID				staff-create
+//	@Accept			json
+//	@Produce		json
+//	@Param			input	body		models.Staff	true	"Staff"
+//	@Success		201		{object}	models.Staff
+//	@Failure		default	{object}	models.Message
+//	@Router			/staff [post]
 func (h *Handlers) staffCreate(c *gin.Context) {
 	staff := models.Staff{}
 	if err := c.BindJSON(&staff); err != nil {
@@ -31,6 +42,17 @@ func (h *Handlers) staffCreate(c *gin.Context) {
 	c.JSON(http.StatusOK, staff)
 }
 
+//	@Summary		StaffGetByID
+//	@Security		JWTAuth
+//	@Tags			Staff
+//	@Description	returns an employee by id
+//	@ID				staff-get-by-id
+//	@Accept			json
+//	@Produce		json
+//	@Success		200		{object}	models.Staff
+//	@Failure		default	{object}	models.Message
+//	@Router			/staff/{id} [get]
+//	@Param			id	path	string	true	"Staff ID"
 func (h *Handlers) staffGetByID(c *gin.Context) {
 	id := uuid.Parse(c.Param("id"))
 	admin := c.MustGet("Admin").(models.Admin)
@@ -53,6 +75,16 @@ func (h *Handlers) staffGetByID(c *gin.Context) {
 	c.JSON(http.StatusOK, staff)
 }
 
+//	@Summary		StaffGetAllInRestaurant
+//	@Security		JWTAuth
+//	@Tags			Staff
+//	@Description	returns all staff in a restaurant
+//	@ID				staff-get-all
+//	@Accept			json
+//	@Produce		json
+//	@Success		200		{object}	models.Staff
+//	@Failure		default	{object}	models.Message
+//	@Router			/staff [get]
 func (h *Handlers) staffGetInRestaurant(c *gin.Context) {
 	admin := c.MustGet("Admin").(models.Admin)
 	staff, err := h.service.StaffGetAllInRestaurantService(admin)
@@ -74,6 +106,17 @@ func (h *Handlers) staffGetInRestaurant(c *gin.Context) {
 	c.JSON(http.StatusOK, staff)
 }
 
+//	@Summary		StaffUpdate
+//	@Security		JWTAuth
+//	@Tags			Staff
+//	@Description	updates an employee
+//	@ID				staff-update
+//	@Accept			json
+//	@Produce		json
+//	@Param			input	body		models.Staff	true	"Staff"
+//	@Success		204		{object}	models.Staff
+//	@Failure		default	{object}	models.Message
+//	@Router			/staff [put]
 func (h *Handlers) staffUpdate(c *gin.Context) {
 	admin := c.MustGet("Admin").(models.Admin)
 	var staff models.Staff 
@@ -101,6 +144,17 @@ func (h *Handlers) staffUpdate(c *gin.Context) {
 	c.JSON(http.StatusNoContent, nil)
 }
 
+//	@Summary		StaffDelete
+//	@Security		JWTAuth
+//	@Tags			Staff
+//	@Description	deletes an employee
+//	@ID				staff-delete
+//	@Accept			json
+//	@Produce		json
+//	@Success		204		{object}	models.Staff
+//	@Failure		default	{object}	models.Message
+//	@Router			/staff/{id} [delete]
+//	@Param			id	path	string	true	"Staff ID"
 func (h *Handlers) staffDelete(c *gin.Context) {
 	id := c.Param("id")
 	admin := c.MustGet("Admin").(models.Admin)
