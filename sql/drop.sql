bf919210a2181eaf61f36616303a687dbd74ba94
Max Ostapchuck <mksmstpck@gmail.com>
2023-09-25 20:01:14 +0300

    make it work

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,7 +1,7 @@
+drop table if exists reserv;
 drop table if exists tables;
 drop table if exists staffs;
 drop table if exists restaurants;
 drop table if exists dishes;
 drop table if exists admins;
 drop table if exists menus;
-drop table if exists resercs;
