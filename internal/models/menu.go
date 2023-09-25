db1a8312267d33d32e3252a36a925408125f40d6
Max Ostapchuck <mksmstpck@gmail.com>
2023-09-25 22:54:37 +0300

    feat(internal.models): change qrcode id type

--- internal/models/menu.go
+++ internal/models/menu.go
@@ -1,13 +1,11 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type Menu struct {
 	ID           string  `json:"id" bun:"id,pk"`
 	Name         string  `json:"name" binding:"required"`
 	Description  string  `json:"description" binding:"required"`
-	Dishes      []*Dish `json:"dish" bun:"rel:has-many,join:id=menu_id"`
-	QRCodeID       primitive.ObjectID  `json:"qrcodeID" bun:"qrcode"`
-	QRCodeBytes []byte `json:"qrcode"`
+	Dishes       []*Dish `json:"dish" bun:"rel:has-many,join:id=menu_id"`
+	QRCodeID     string  `json:"qrcodeID" bun:"qrcode"`
+	QRCodeBytes  []byte `json:"qrcode" bun:"-"`
 	RestaurantID string  `json:"restaurant_id" bun:"restaurant_id"`
 }
