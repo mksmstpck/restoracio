3c326b682b992ae2819376f8224bca2fb1991a9d
Max Ostapchuck <mksmstpck@gmail.com>
2023-09-25 20:04:42 +0300

    feat(internal.models): prepare model to qrcode saving

--- internal/models/menu.go
+++ internal/models/menu.go
@@ -1,10 +1,13 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Menu struct {
 	ID           string  `json:"id" bun:"id,pk"`
 	Name         string  `json:"name" binding:"required"`
 	Description  string  `json:"description" binding:"required"`
 	Dishes      []*Dish `json:"dish" bun:"rel:has-many,join:id=menu_id"`
-	QRCode       []byte  `json:"qrcode" bun:"qrcode"`
+	QRCodeID       primitive.ObjectID  `json:"qrcodeID" bun:"qrcode"`
+	QRCodeBytes []byte `json:"qrcode"`
 	RestaurantID string  `json:"restaurant_id" bun:"restaurant_id"`
 }
