068655621a338e068e549af40ed47d3d4710ec23
Max Ostapchuck <mksmstpck@gmail.com>
2023-12-08 14:46:07 +0200

    feat(utils): add papper

--- utils/passwordHash.go
+++ utils/passwordHash.go
@@ -1,19 +1,26 @@
 package utils
 
 import (
+	"crypto/sha1"
 	"math/rand"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+const symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=,./;[]{}!@#$%^&*()_+"
+
 func PasswordHash(password string) (string, string) {
-	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=,./;[]{}!@#$%^&*()_+"
+	papper := string(symbols[rand.Intn(len(symbols))])
 	salt := make([]byte, 10)
     for i := range salt {
-        salt[i] = letterBytes[rand.Intn(len(letterBytes))]
+        salt[i] = symbols[rand.Intn(len(symbols))]
     }
 
-	hash, err := bcrypt.GenerateFromPassword([]byte(password+string(salt)), bcrypt.DefaultCost)
+	hashObj := sha1.New()
+	hashObj.Write([]byte(password))
+	passwordHash := hashObj.Sum(nil)
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(string(passwordHash) + papper + string(salt)), 10)
 	if err != nil {
 		panic(err)
 	}
@@ -21,6 +28,18 @@ func PasswordHash(password string) (string, string) {
 }
 
 func CheckPasswordHash(password, hash, salt string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password+salt))
-	return err == nil
+	hashObj := sha1.New()
+	hashObj.Write([]byte(password))
+	passwordHash := hashObj.Sum(nil)
+
+	for i := 0; i < len(symbols); i++ {
+		err := bcrypt.CompareHashAndPassword(
+			[]byte(hash),
+			[]byte(string(passwordHash) + string(symbols[i]) + salt),
+		)
+		if (err == nil) == true {
+			return true
+		} 
+	}
+	return false
 }
