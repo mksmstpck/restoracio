ab0c64b999dbec575282bd25584ab0a04bc1c77a
mksmstpck <mksmstpck>
2023-12-27 16:27:49 +0200

    feat(internal.dto): add some changes

--- internal/dto/restaurant.go
+++ internal/dto/restaurant.go
@@ -3,12 +3,12 @@ package dto
 import "github.com/uptrace/bun"
 
 type Restaurant struct {
-	bun.BaseModel 		
-	ID       string   
-	Name     string  
-	Location string  
-	AdminID  string  
-	Staff    []*Staff	
-	Menu     *Menu  	
-	Tables   []*Table	
+	bun.BaseModel
+	ID       string
+	Name     string
+	Location string
+	AdminID  string
+	Staff    []*Staff
+	Menu     *Menu
+	Tables   []*Table
 }
--- internal/dto/table.go
+++ internal/dto/table.go
@@ -2,11 +2,11 @@ package dto
 
 type Table struct {
 	ID           string
-	Number       int   
+	Number       int
 	Placement    string
-	MaxPeople    int   
-	IsReserved   bool  
-	IsOccupied   bool  
-	RestaurantID string 
+	MaxPeople    int
+	IsReserved   bool
+	IsOccupied   bool
+	RestaurantID string
 	Reservation  *Reserv
 }
