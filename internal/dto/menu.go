7b41953f2c3e5b9a0342ea3468fa0567a90dd5f3
mksmstpck <mksmstpck>
2023-12-30 12:54:54 +0200

    feat(internal.dto): add qrcode field to Menu

--- internal/dto/menu.go
+++ internal/dto/menu.go
@@ -1,9 +1,11 @@
 package dto
 
 type Menu struct {
-	ID           string 
-	Name         string 
-	Description  string 
+	ID           string
+	Name         string
+	Description  string
+	Dishes       []*Dish
 	QRCodeID     string
+	QRCode       []byte
 	RestaurantID string
 }
